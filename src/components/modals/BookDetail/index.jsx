aa3a304debc6bab9e83b3446718d1f5d712d767f
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-16 18:42:27 -0300

    feature: created book detail modal

---
+++ src/components/modals/BookDetail/index.jsx
@@ -0,0 +1,106 @@
+import Modal from 'react-modal'
+import PropTypes from 'prop-types'
+import { isMatch, format, parse } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import placeholder from '../../../assets/img/placeholder.jpg'
+import {
+  Image,
+  Title,
+  Info,
+  Snippet,
+  Card,
+  CardContent,
+  CardPreview,
+  CardActions,
+  DetailButton,
+  FavButton,
+} from './styles'
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+}
+
+const formatAuthors = (authors = []) => {
+  if (authors.length > 1) {
+    const last = authors.pop()
+    return authors.join(', ').concat(' e ', last)
+  }
+  if (authors.length) return authors[0]
+  return 'Autor Desconhecido'
+}
+
+const formatThumbnail = (thumb) => {
+  if (thumb) return thumb
+  return placeholder
+}
+
+const formatPublishedDate = (dateString = '') => {
+  if (isMatch(dateString, 'yyyy-MM-dd')) {
+    const date = parse(dateString, 'yyyy-MM-dd', new Date())
+    return format(date, 'dd MMMM yyyy', { locale: ptBR })
+  }
+  if (isMatch(dateString, 'yyyy')) return dateString
+  return 'Data de publicação indisponível'
+}
+
+const formatPageCount = (pages) => {
+  if (pages) return `${pages} páginas`
+  return 'Não informado'
+}
+
+Modal.setAppElement('#root')
+
+const BookDetail = (props) => {
+  const { isOpen, closeModal, book } = props
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel='Example Modal'
+    >
+      <Card>
+        <CardPreview>
+          <Image
+            src={formatThumbnail(book.volumeInfo?.imageLinks?.thumbnail)}
+            alt='Book'
+          />
+        </CardPreview>
+        <CardContent>
+          <Title>{book.volumeInfo?.title}</Title>
+          <Info>
+            por {formatAuthors(book.volumeInfo?.authors)} |{' '}
+            {formatPublishedDate(book.volumeInfo?.publishedDate)}
+          </Info>
+          <Snippet>
+            {formatPageCount(book.volumeInfo?.pageCount)} |{' '}
+            {book.volumeInfo?.publisher}
+          </Snippet>
+          <Snippet>{book.volumeInfo?.description}</Snippet>
+        </CardContent>
+        <CardActions>
+          <DetailButton onClick={() => closeModal()}>Fechar</DetailButton>
+          <FavButton onClick={() => console.log('adicionar aos favoritos')}>
+            Adicionar aos favoritos
+          </FavButton>
+        </CardActions>
+      </Card>
+    </Modal>
+  )
+}
+
+BookDetail.propTypes = {
+  isOpen: PropTypes.bool,
+  book: PropTypes.object,
+  closeModal: PropTypes.func,
+}
+
+export default BookDetail
---
+++ src/components/modals/BookDetail/styles.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  background-color: #fff;
+  width: 100%;
+  max-width: 600px;
+  margin-bottom: 10px;
+`
+
+export const CardPreview = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 12px;
+`
+
+export const CardContent = styled.div`
+  padding: 2px 16px;
+`
+
+export const CardActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 0.25rem;
+  margin-bottom: 0.25rem;
+`
+
+export const Image = styled.img`
+  width: 100%;
+  max-width: 140px;
+  height: 100%;
+  border: 1px solid #eee;
+`
+
+export const Title = styled.h1`
+  font-weight: bold;
+  font-size: 1.25rem;
+  margin: 0.75rem 0 0.5rem;
+`
+
+export const Info = styled.h2`
+  font-weight: 500;
+  font-size: 1.125rem;
+  margin: 0.25rem 0 0.5rem;
+  display: inline-block;
+`
+
+export const Snippet = styled.p`
+  font-weight: 400;
+  font-size: 1rem;
+  margin-top: 0.5rem;
+`
+
+export const DetailButton = styled.button`
+  background-color: #3b82f6;
+  border: none;
+  color: white;
+  padding: 0.25rem 0.75rem;
+  border-radius: 0.75rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.3s;
+  &:hover {
+    background-color: #2563eb;
+  }
+`
+
+export const FavButton = styled.button`
+  background-color: #f59e0b;
+  border: none;
+  color: white;
+  padding: 0.25rem 0.75rem;
+  border-radius: 0.75rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.3s;
+  &:hover {
+    background-color: #d97706;
+  }
+`
