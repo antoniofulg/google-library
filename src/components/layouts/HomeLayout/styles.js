6d0c0ee2feea49970f1fca006f8558698b370ccc
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-16 16:33:58 -0300

    fix: updated background and title

--- src/components/layouts/HomeLayout/styles.js
+++ src/components/layouts/HomeLayout/styles.js
@@ -1,12 +1,19 @@
 import styled from 'styled-components'
 
 export const Background = styled.div`
-  background-color: #ddd;
-  height: 100vh;
+  background: rgb(230, 230, 230);
+  background: linear-gradient(
+    180deg,
+    rgba(230, 230, 230, 1) 0%,
+    rgba(255, 255, 255, 1) 100%
+  );
+  min-height: 100vh;
+  padding-top: 1rem;
+  padding-bottom: 2rem;
 `
 
 export const Title = styled.h1`
   font-size: 2rem;
   text-align: center;
-  color: #1f2937;
+  color: #555;
 `
