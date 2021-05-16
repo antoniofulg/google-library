773548617dbc9827b9ad48c140b7d34b69417069
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-16 18:42:51 -0300

    feature: added book detail modal

--- src/components/cards/CardsList/index.jsx
+++ src/components/cards/CardsList/index.jsx
@@ -1,14 +1,45 @@
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import BookCard from '../BookCard'
 import Pagination from '../../tools/Pagination'
 import Row from '../../layouts/Row'
+import BookDetailModal from '../../modals/BookDetail'
 import { CardsListDiv, SearchInfo } from './styles'
 
 const CardsList = (props) => {
   const { books, meta, getByPage } = props
+  const [modalIsOpen, setIsOpen] = useState(false)
+  const [selectedBook, setSelectedBook] = useState({})
+
+  useEffect(() => {
+    openModal()
+  }, [selectedBook])
+
+  const selectBook = (book) => {
+    console.log('selectBook', book)
+    setSelectedBook(book)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
 
   const getResultsInfo = (meta) => {
     return `${meta.totalItems} livros encontrados`
+  }
+
+  const Modal = (item) => {
+    return (
+      <BookDetailModal
+        isOpen={modalIsOpen}
+        closeModal={closeModal}
+        book={item}
+      ></BookDetailModal>
+    )
   }
 
   return (
@@ -21,13 +52,11 @@ const CardsList = (props) => {
           {books.map((item) => (
             <BookCard
               key={item.id}
-              thumbnail={item.volumeInfo?.imageLinks?.thumbnail}
-              title={item.volumeInfo?.title}
-              author={item.volumeInfo?.authors}
-              textSnippet={item.searchInfo?.textSnippet}
-              publishedDate={item.volumeInfo?.publishedDate}
+              book={item}
+              selectBook={selectBook}
             ></BookCard>
           ))}
+          {selectedBook.id && modalIsOpen ? Modal(selectedBook) : <></>}
         </CardsListDiv>
       </Row>
       <Row>
