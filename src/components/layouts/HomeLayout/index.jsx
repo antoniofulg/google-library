62a62b3d28ce0ecda35b660590d6c4c3341d6e24
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-16 22:53:57 -0300

    feature: added loading and error feedback

--- src/components/layouts/HomeLayout/index.jsx
+++ src/components/layouts/HomeLayout/index.jsx
@@ -1,24 +1,38 @@
-import { Background, Title } from './styles'
+import { ToastContainer } from 'react-toastify'
 import PropTypes from 'prop-types'
 import Container from '../Container'
 import Row from '../Row'
 import Column from '../Column'
+import { Background, Title, Loading, LoadingContainer } from './styles'
 
 const Layout = (props) => {
+  const { title, header, body, loading } = props
+
   return (
-    <Background>
-      <Container>
-        <Row>
-          <Column>
-            <Title>{props.title}</Title>
-          </Column>
-        </Row>
-        <Row>
-          <Column>{props.header}</Column>
-        </Row>
-        {props.body}
-      </Container>
-    </Background>
+    <>
+      <Background>
+        <Container>
+          <Row>
+            <Column>
+              <Title>{title}</Title>
+            </Column>
+          </Row>
+          <Row>
+            <Column>{header}</Column>
+          </Row>
+          {loading ? (
+            <Row>
+              <LoadingContainer>
+                <Loading />
+              </LoadingContainer>
+            </Row>
+          ) : (
+            body
+          )}
+        </Container>
+      </Background>
+      <ToastContainer />
+    </>
   )
 }
 
@@ -26,7 +40,8 @@ Layout.propTypes = {
   title: PropTypes.string,
   header: PropTypes.node,
   body: PropTypes.node,
-  children: PropTypes.node,
+  toast: PropTypes.node,
+  loading: PropTypes.bool,
 }
 
 export default Layout
--- src/components/layouts/HomeLayout/styles.js
+++ src/components/layouts/HomeLayout/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export const Background = styled.div`
   background: rgb(230, 230, 230);
@@ -16,4 +16,36 @@ export const Title = styled.h1`
   font-size: 2rem;
   text-align: center;
   color: #555;
+`
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 2rem;
+`
+
+const loadingAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+export const Loading = styled.div`
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  &:after {
+    content: ' ';
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #888;
+    border-color: #888 transparent #888 transparent;
+    animation: ${loadingAnimation} 1.2s linear infinite;
+  }
 `
