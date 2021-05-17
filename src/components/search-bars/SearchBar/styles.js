3cf82bbef59fea74f97b443db9565364691b35e2
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-17 01:07:51 -0300

    fix: ajust on input padding and border radius

--- src/components/search-bars/SearchBar/styles.js
+++ src/components/search-bars/SearchBar/styles.js
@@ -4,12 +4,12 @@ export const StyledInput = styled.input`
   --tw-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
   box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
     var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
-  padding-left: 1rem;
-  padding-right: 1rem;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
   margin-bottom: 1rem;
   width: 100%;
   display: block;
-  border-radius: 0.75rem;
+  border-radius: 1.75rem;
   background-color: #fff;
   height: 3.5rem;
   -webkit-appearance: textfield;
