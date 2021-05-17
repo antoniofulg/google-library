52cbb159f5cfebd6fd42a5143ebc51d73904e6c1
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-16 22:55:54 -0300

    feature: setup toast and loading indicators

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import HomeLayout from '../../components/layouts/HomeLayout'
 import SearchBar from '../../components/search-bars/SearchBar'
 import CardsList from '../../components/cards/CardsList'
@@ -9,6 +11,7 @@ const Home = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [books, setBooks] = useState([])
   const [meta, setMeta] = useState({})
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     getBooks(searchTerm)
@@ -25,6 +28,7 @@ const Home = () => {
   const getBooks = async (q, startIndex = 0, maxResults = 8) => {
     if (q) {
       try {
+        setLoading(true)
         q = `intitle:${q}`
         const { totalItems, items } = await findBooks({
           q,
@@ -37,11 +41,17 @@ const Home = () => {
           startIndex,
           maxResults: 8,
         })
+        window.scrollTo(0, 0)
+        setLoading(false)
       } catch (error) {
-        console.log(error)
+        setLoading(false)
+        toast('Não foi possível realizar a busca!')
       }
+    } else {
+      toast.warning('Preencha o campo de busca!')
     }
   }
+
   const showCardList = () => {
     if (meta.totalItems > 0) {
       return (
@@ -54,6 +64,7 @@ const Home = () => {
   return (
     <HomeLayout
       title='Busque por seus livros favoritos!'
+      loading={loading}
       header={
         <SearchBar
           getBySearchTerm={getBySearchTerm}
